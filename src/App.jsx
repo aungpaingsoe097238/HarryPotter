6c730631e5cff813d71eceb88b2d38cb5760d405
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-03-11 20:00:11 +0630

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import yoga from "./assets/images/pngwing.com.png";
+import computer from "./assets/images/computer3.png";
 
 function App() {
-
   return (
-    <div>
-      Hello World!
+    <div className=" h-screen flex justify-center items-center">
+      <div className=" relative">
+        <img src={yoga} className=" h-[400px] w-full " alt="" />
+        <div className=" absolute bottom-[30%] left-[30%]">
+          <img src={computer} className=" h-[100px] " alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
