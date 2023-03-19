9f07d98eeb76bf7d695bf8c730ccf9279ffaaf99
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-03-19 20:03:53 +0630

    ball

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ import Wing from "./Wing";
 const Hero = () => {
   return (
     <div className=" h-screen flex justify-center items-center">
-      {/* <div className=" relative ">
-        <img src={harry} className=" h-[400px] w-full " alt="" />
-        <div className="absolute top-2 "> */}
+      <div className=" relative ">
+        <img src={harry} className=" h-[400px] w-full updown" alt="" />
+        <div className="absolute top-2 -left-10 ">
           <Wing />
-        {/* </div>
-      </div> */}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Wing.jsx
+++ src/components/Wing.jsx
@@ -7,20 +7,11 @@ import Ball from "../assets/images/ball.png";
 
 const Wing = () => {
   return (
-    <div>
-      {/* Version One */}
-      {/* <div className=" relative ">
-        <div className=" w-[100px] h-[2px] bg-yellow-500 rounded-full shadow-lg fly"></div>
-        <div className=" w-[70px] h-[1px] mx-auto bg-yellow-500 rounded-full shadow-lg fly"></div>
-        <div className=" w-[50px] h-[1px] mx-auto bg-yellow-500 rounded-full shadow-lg fly"></div>
-        <div className=" flex justify-center items-center absolute top-[-6px] left-[35%] ">
-          <AiFillHtml5 className=" text-3xl text-white p-1 rounded-full bg-yellow-500"/>
-        </div> 
-      </div> */}
+    <div className=" wing">
       <div className=" relative">
         <img src={WingS} className="fly h-[80px]" alt="" />
-        <div className=" flex justify-center items-center absolute bottom-[10%] left-[13%] rounded-full ">
-          <img src={ Ball } className=" h-[60px] " alt="" />
+        <div className=" flex justify-center items-center absolute bottom-[15%] left-[13%] rounded-full ">
+          <img src={ Ball } className=" h-[60px]" alt="" />
         </div>
       </div>
     </div>
