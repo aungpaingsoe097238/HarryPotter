8d027a3c8605dc787b3bb7177b7850edf46df358
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-03-22 23:00:58 +0630

    circle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,22 +3,12 @@ import "../assets/hero.css";
 import HeroImage from "./HeroImage";
 import HeroText from "./HeroText";
 import HeroTitle from "./HeroTitle";
-import cloud from "../assets/images/cloud.png";
-import cloud2 from "../assets/images/cloud2.png";
-import cloud3 from "../assets/images/cloud3.png";
-import cloud4 from "../assets/images/cloud4.png";
-import cloud5 from "../assets/images/cloud5.png";
-import cloud6 from "../assets/images/cloud6.png";
-import cloud7 from "../assets/images/cloud7.png";
-import cloud8 from "../assets/images/cloud8.png";
+import world from "../assets/images/world2.png";
 
 const Hero = () => {
   return (
-    <div className="h-screen relative flex hero ">
-      <div className=" absolute bottom-[-70%] left-[22%] ">
-        <div className=" w-[1000px] h-[1000px] bg-rose-500 rounded-full ">
-        </div>
-      </div>
+    <div className="h-screen relative flex hero  overflow-hidden ">
+      <div className="circle bg-black50 "></div>
       <div className=" basis-1/3 ">
         <HeroTitle />
       </div>
